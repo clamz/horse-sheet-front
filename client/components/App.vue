0ba695648b8ddc990cba44a4f676dea14fa9816f
Damien GUEZOU <damien.guezou@gmail.com>
2016-11-12 10:49:24 +0100

    clean repo

--- client/components/App.vue
+++ client/components/App.vue
@@ -1,16 +1,13 @@
 <template>
   <div id="app">
-    <header-view></header-view>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HeaderView from '../components/Header'
 
 export default {
   components: {
-    HeaderView
   }
 }
 </script>
